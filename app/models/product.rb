d27ef11e12acd69815bd8ce421cc27680eb9a692
Kyle Swaffield <kyle@swaff.id.au>
2020-12-21 21:07:55 +1100

    iteration-d2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ApplicationRecord
   default_scope{ order( title: :desc) }
+  has_many :line_items
+  before_destroy :ensure_not_refereneced_by_any_line_item
+
+  # ensure that there are no line items referencing this Product
+  def ensure_not_refereneced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 
 
 
